7325714b288e984b42eaebf31f6e1d1f49a94cb1
charles <charleszbis@gmail.com>
2022-12-15 21:30:05 +0100

    d03 is finished

--- day05/ex03/Intern.cpp
+++ day05/ex03/Intern.cpp
@@ -16,14 +16,14 @@ AForm	*Intern::makeForm(const std::string &name, const std::string &target)
 {
 	int i;
 	std::string	formName[] = {"PresidentialPardonForm", "RobotomyRequestForm", "ShrubberyCreationForm"};
-	for (i = 0; i < 4 && name != formName[i]; i++);
+	for (i = 0; i < 3 && name != formName[i]; i++);
 	switch (i)
 	{
-		case 1:
+		case 0:
 			return (new PresidentialPardonForm(target));
-		case 2:
+		case 1:
 			return (new RobotomyRequestForm(target));
-		case 3:
+		case 2:
 			return (new ShrubberyCreationForm(target));
 		default:
 			throw InternMakeException();
